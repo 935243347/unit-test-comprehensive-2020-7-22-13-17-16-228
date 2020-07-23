2c8b55dd968b2c9fa04c7596d18ff26736ca7696
Max Miao <miaoma2@oocl.com>
2020-07-23 20:53:10 +0800

    [GuessNumber][Max] feature: pass should_uncontain_repeat_number_when_generate_given_iscalled

--- src/main/java/game/AnswerGeneratorImpl.java
+++ src/main/java/game/AnswerGeneratorImpl.java
@@ -1,12 +1,22 @@
 package game;
 
+import java.util.HashSet;
+
 public class AnswerGeneratorImpl implements AnswerGenerator{
     @Override
     public int[] generate() {
         int[] answer = new int[4];
-        for(int i = 0; i < 4; i++){
+        HashSet<Integer> integerSet = new HashSet<Integer>();
+        while(true){
             int randomNum = (int) (Math.random() * 10);
-            answer[i]=randomNum;
+            integerSet.add(randomNum);
+            if(integerSet.size()==4){
+                break;
+            }
+        }
+        Object[] objects = integerSet.toArray();
+        for(int i = 0; i < objects.length; i++){
+            answer[i]=(int)objects[i];
         }
         return answer;
     }
