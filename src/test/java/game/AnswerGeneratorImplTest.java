8fec518fcba82c8de992ac8418c06f6134424142
Max Miao <miaoma2@oocl.com>
2020-07-23 20:28:28 +0800

    [GuessNumber][Max] test: test should_uncontain_repeat_number_when_generate_given_iscalled

--- src/test/java/game/AnswerGeneratorImplTest.java
+++ src/test/java/game/AnswerGeneratorImplTest.java
@@ -2,6 +2,8 @@ package game;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.HashSet;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AnswerGeneratorImplTest {
@@ -10,5 +12,17 @@ public class AnswerGeneratorImplTest {
         AnswerGenerator answerGenerator = new AnswerGeneratorImpl();
         int[] result = answerGenerator.generate();
         assertEquals(4,result.length);
+    }
+    @Test
+    void should_uncontain_repeat_number_when_generate_given_iscalled() {
+        AnswerGenerator answerGenerator = new AnswerGeneratorImpl();
+        for(int i = 0; i < 10; i++){
+            int[] result = answerGenerator.generate();
+            HashSet<Integer> integerSet = new HashSet<Integer>();
+            for(int j: result){
+                integerSet.add(j);
+            }
+            assertEquals(4,integerSet.size());
+        }
     }
 }
