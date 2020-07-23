ccccbae7e398a4f35439b9fb2019b8f1dde399d4
Max Miao <miaoma2@oocl.com>
2020-07-23 21:47:55 +0800

    [GuessNumber][Max] feature: pass should_return_true_when_valid_given_input_number_1567a

--- src/main/java/game/InputValidator.java
+++ src/main/java/game/InputValidator.java
@@ -1,7 +1,25 @@
 package game;
 
+import java.util.HashSet;
+
 public class InputValidator {
-    public boolean valid(String s) {
-        return true;
+    public boolean valid(String inputNumber) {
+        String number = inputNumber.replaceAll(" ", "");
+        char[] inputNumberCharArray = number.toCharArray();
+        if(inputNumberCharArray.length == 4) {
+            int[] intArry = new int[4];
+            for (int i = 0; i < inputNumberCharArray.length; i++) {
+                intArry[i] = inputNumberCharArray[i] - '0';
+            }
+            HashSet<Integer> integerSet = new HashSet<Integer>();
+            for(int j: intArry){
+                integerSet.add(j);
+            }
+            if(integerSet.size() == 4){
+                return true;
+            }
+        }
+        System.out.println("Wrong Input，Input again");
+        return false;
     }
 }
--- src/test/java/game/InputValidatorTest.java
+++ src/test/java/game/InputValidatorTest.java
@@ -2,13 +2,33 @@ package game;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class InputValidatorTest {
     @Test
     void should_return_true_when_valid_given_input_number_1567() {
         InputValidator inputValidator = new InputValidator();
-        boolean valid = inputValidator.valid("1567");
+        boolean valid = inputValidator.valid("1 5 6 7");
         assertTrue(valid);
     }
+    @Test
+    void should_return_true_when_valid_given_input_number_1566() {
+        InputValidator inputValidator = new InputValidator();
+        boolean valid = inputValidator.valid("1 5 6 6");
+        assertFalse(valid);
+    }
+    @Test
+    void should_return_true_when_valid_given_input_number_15676() {
+        InputValidator inputValidator = new InputValidator();
+        boolean valid = inputValidator.valid("1 5 6 7 6");
+        assertFalse(valid);
+    }
+    @Test
+    void should_return_true_when_valid_given_input_number_1567a() {
+        InputValidator inputValidator = new InputValidator();
+        boolean valid = inputValidator.valid("1 5 6 7 a");
+        assertFalse(valid);
+    }
+
 }
