d6aebb4315d3ba3d51f1f567d571ec8407f6635e
Max Miao <miaoma2@oocl.com>
2020-07-27 20:06:17 +0800

    [GuessNumber][Max] test: test should_return_false_when_valid_given_input_number_negative_1567

--- src/test/java/game/InputValidatorTest.java
+++ src/test/java/game/InputValidatorTest.java
@@ -13,22 +13,27 @@ public class InputValidatorTest {
         assertTrue(valid);
     }
     @Test
-    void should_return_true_when_valid_given_input_number_1566() {
+    void should_return_false_when_valid_given_input_number_1566() {
         InputValidator inputValidator = new InputValidator();
         boolean valid = inputValidator.validNumber("1 5 6 6");
         assertFalse(valid);
     }
     @Test
-    void should_return_true_when_valid_given_input_number_15676() {
+    void should_return_false_when_valid_given_input_number_15676() {
         InputValidator inputValidator = new InputValidator();
         boolean valid = inputValidator.validNumber("1 5 6 7 6");
         assertFalse(valid);
     }
     @Test
-    void should_return_true_when_valid_given_input_number_1567a() {
+    void should_return_false_when_valid_given_input_number_1567a() {
         InputValidator inputValidator = new InputValidator();
         boolean valid = inputValidator.validNumber("1 5 6 7 a");
         assertFalse(valid);
     }
-    //todo: -1   10  null
+    @Test
+    void should_return_false_when_valid_given_input_number_negative_1567() {
+        InputValidator inputValidator = new InputValidator();
+        boolean valid = inputValidator.validNumber("-1 5 6 7");
+        assertFalse(valid);
+    }
 }
