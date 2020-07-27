0e215b05f8365bb8c2fae785a1d202ff8cb14789
Max Miao <miaoma2@oocl.com>
2020-07-27 19:52:01 +0800

    [GuessNumber][Max] refactor: refactor generate function about generate num

--- src/main/java/game/AnswerGeneratorImpl.java
+++ src/main/java/game/AnswerGeneratorImpl.java
@@ -1,22 +1,18 @@
 package game;
 
-import java.util.HashSet;
+import java.util.*;
 
-public class AnswerGeneratorImpl implements AnswerGenerator{
+public class AnswerGeneratorImpl implements AnswerGenerator {
     @Override
     public int[] generate() {
         int[] answer = new int[4];
-        HashSet<Integer> integerSet = new HashSet<Integer>();
-        while(true){
-            int randomNum = (int) (Math.random() * 10);
-            integerSet.add(randomNum);
-            if(integerSet.size()==4){
-                break;
-            }
+        List<Integer> numberList = new ArrayList<>();
+        for (int num = 0; num < 10; num++) {
+            numberList.add(num);
         }
-        Object[] objects = integerSet.toArray();
-        for(int i = 0; i < objects.length; i++){
-            answer[i]=(int)objects[i];
+        Collections.shuffle(numberList);
+        for(int index = 0; index < 4; index++){
+            answer[index] = numberList.get(index);
         }
         return answer;
     }
